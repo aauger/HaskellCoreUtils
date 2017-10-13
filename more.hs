2938d5fb44579b84a0efe62b7009ca3381a8ea39
aauger <augera09@students.rowan.edu>
2017-10-13 19:12:00 -0400

    added more
    this implementation of more supports user-supplied line counts, or defaults to 20 lines

---
+++ more.hs
@@ -0,0 +1,25 @@
+import System.Environment
+import Control.Monad
+
+fShow :: [String] -> Int -> IO ()
+fShow [] _ = return ()
+fShow xs n = do
+	forM_ (take n xs) putStrLn
+	getChar
+	fShow (drop n xs) n
+
+main = do
+	args <- getArgs
+	case args of
+		[n, fn] -> do
+			contents <- readFile fn
+			let fFileLines = lines contents
+			let nv = read n :: Int
+			fShow fFileLines nv
+		[fn] -> do
+			contents <- readFile fn
+			let fFileLines = lines contents
+			fShow fFileLines 20
+		_    -> do
+			putStrLn "Usage: more [optional: linecount] [filename]"
+
