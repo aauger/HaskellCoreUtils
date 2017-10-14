065abf8eabb4b07105c814c2982f909113e7e529
aauger <augera09@students.rowan.edu>
2017-10-14 18:25:07 -0400

    added cat
    doesn't accept options, only accepts one (or more) files to display.

---
+++ src/cat.hs
@@ -0,0 +1,18 @@
+import System.Environment
+import Control.Monad
+
+displayLines :: String -> IO ()
+displayLines fn = do
+	content <- readFile fn
+	let fLines = lines content
+	forM_ fLines putStrLn
+
+displayFiles :: [String] -> IO ()
+displayFiles [] = return ()
+displayFiles xs = forM_ xs displayLines
+
+main = do
+	args <- getArgs
+	case args of
+		[] -> putStrLn "Usage: cat file [optional: additional files]"
+		xs -> displayFiles xs
