613b528368f6787eb8d8572bfbb8232b7b39d85f
aauger <augera09@students.rowan.edu>
2017-10-17 13:58:04 -0400

    added tee
    tee is used to write stdin to a file, then pass it forward (by writing to stdout).
    
    useful when a user would like to save the output of some program that's in the middle of a pipeline

---
+++ src/tee.hs
@@ -0,0 +1,13 @@
+import System.Environment
+import System.IO
+import Control.Monad
+
+main = do
+	args <- getArgs
+	case args of
+		[] -> do
+			putStrLn "usage: tee filename [optional: additional filenames]"
+		xs -> do
+			pString <- getContents
+			forM_ xs $ (flip writeFile) pString
+			putStr pString
