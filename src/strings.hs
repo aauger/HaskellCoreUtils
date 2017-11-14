8453bbc64f1870850d92036fa44fc15cf0d1f2ec
aauger <augera09@students.rowan.edu>
2017-11-14 18:33:15 -0500

    strings
    GNU binutil that finds strings of at least the specified length in binaries (or really, any other file type)

---
+++ src/strings.hs
@@ -0,0 +1,25 @@
+import System.Environment
+import Data.Maybe
+import Data.List
+import Control.Monad
+import Data.Char
+
+split :: Eq a => a -> [a] -> [[a]]
+split _ [] = []
+split s w = takeWhile (/= s) w : (split s $ drop 1 $ dropWhile (/= s) w)
+
+processFragment :: String -> String
+processFragment inp = reverse $ takeWhile (\a -> not (isControl a)) $ reverse inp
+
+main = do
+	args <- getArgs
+	case args of 
+		[a, b] -> do
+			contents <- readFile b
+			let minLength = read a :: Int
+			let strings = filter (\a -> length a > minLength) 
+				$ map processFragment 
+				$ split '\0' contents
+			forM_ strings putStrLn
+		_      ->
+			putStrLn "Usage: strings [min-length: 4?] [binary]"
