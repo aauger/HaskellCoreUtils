b74373c1fa26f95ccdbcde298bffae863c8110b4
aauger <augera09@students.rowan.edu>
2017-11-16 18:13:17 -0500

    fixed hexdump

--- HaskellCoreUtils/src/hexdump.hs
+++ HaskellCoreUtils/src/hexdump.hs
@@ -4,24 +4,27 @@ import System.Environment
 import Control.Monad
 import Numeric (showHex)
 import Data.Char
-import qualified Data.ByteString.Char8 as C
+import Data.Word
+import qualified Data.ByteString.Lazy as C
 
-printDump :: C.ByteString -> IO ()
+printDump :: [Word8] -> IO ()
 printDump s
-	| s == C.empty = return ()
+	| s == [] = return ()
 	| otherwise = do
-		C.putStr $ C.concatMap (\a -> (C.pack (hpad (showHex (ord a) "") '0' 2))) $ C.take 16 s
+		mapM_ putStr $ map format (take 16 s)
 		putStrLn ""
-		printDump $ C.drop 16 s
+		printDump $ drop 16 s
 		where
 			hpad :: String -> Char -> Int -> String
 			hpad str c i = (replicate (i - (length str)) c) ++ str ++ " "
+			format :: Word8 -> String
+			format i = hpad (showHex i "") ' ' 2
 
 main = do
 	args <- getArgs
 	case args of
 		[fn] -> do
 			contents <- C.readFile fn
-			printDump contents
+			printDump (C.unpack contents)
 		_    -> do
 			putStrLn "Usage: hexdump [filename]"
