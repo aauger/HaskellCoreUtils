63d57def7f0f131a071cd683b3e43618c7892985
aauger <augera09@students.rowan.edu>
2017-11-16 13:59:13 -0500

    Rewrote hexdump to use more efficient bytestring (sort of)
    Bytestring is difficult and confusing.

--- HaskellCoreUtils/src/hexdump.hs
+++ HaskellCoreUtils/src/hexdump.hs
@@ -4,21 +4,24 @@ import System.Environment
 import Control.Monad
 import Numeric (showHex)
 import Data.Char
+import qualified Data.ByteString.Char8 as C
 
-printDump :: String -> IO ()
-printDump [] = return ()
-printDump s = do
-	putStrLn $ concat $ map (\a -> (lpad (showHex (ord a) "") '0' 2) ++ " ") $ take 16 s
-	printDump $ drop 16 s
-	where
-		lpad :: String -> Char -> Int -> String
-		lpad str c i = (replicate (i - (length str)) c) ++ str
+printDump :: C.ByteString -> IO ()
+printDump s
+	| s == C.empty = return ()
+	| otherwise = do
+		C.putStr $ C.concatMap (\a -> (C.pack (hpad (showHex (ord a) "") '0' 2))) $ C.take 16 s
+		putStrLn ""
+		printDump $ C.drop 16 s
+		where
+			hpad :: String -> Char -> Int -> String
+			hpad str c i = (replicate (i - (length str)) c) ++ str ++ " "
 
 main = do
 	args <- getArgs
 	case args of
 		[fn] -> do
-			contents <- readFile fn
+			contents <- C.readFile fn
 			printDump contents
 		_    -> do
 			putStrLn "Usage: hexdump [filename]"
